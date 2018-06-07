6584c3dbed0cf286235a7d70e8b1df52dfbe5320
a.nurzhanuli@gmail.com <abekkhozha@greet-go.com>
2018-06-07 12:07:39 +0600

    delete ConnectionUtils

--- sandbox.db/src/kz/greetgo/sandbox/db/migration/util/ConnectionUtils.java
+++ sandbox.db/src/kz/greetgo/sandbox/db/migration/util/ConnectionUtils.java
@@ -6,6 +6,7 @@ import kz.greetgo.conf.SysParams;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
+//TODO: удали, если не используешь
 public class ConnectionUtils {
     public static Connection getPostgresAdminConnection() throws Exception {
         Class.forName("org.postgresql.Driver");
